aebdaf6adf1d746136f4f49f375855daddf2c288
Victor Arowo <arowovici@gmail.com>
2019-09-30 20:14:27 +0100

    Added Assets and Designs for Home Page

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  background-image: url('img/scroll2.png');
+  background-repeat: no-repeat;
+  background-size: 900px 700px;
+  background-position: center;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+
+  .title {
+    margin-top: -50px;
+    font-size: 56px;
+  }
+
+  img {
+    width: 130px;
+    height: 130px;
+    transition: transform 0.3s;
+
+    &:hover {
+      cursor: pointer;
+      transform: scale(1.07);
+    }
+  }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Div>
+        <h1 className="title">Welcome to Hogwarts</h1>
+        <h3>Click on the hat to begin sorting.</h3>
+        <img src="img/hat2.png" />
+      </Div>
     );
   }
 }
